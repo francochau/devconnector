fef130796441bc0009bd59bb9d82580efc79a7f5
Franco <franco0506@gmail.com>
2020-08-24 19:15:39 +0800

    Fix Edit Form

--- client/src/components/profile-form/EditProfile.js
+++ client/src/components/profile-form/EditProfile.js
@@ -4,53 +4,87 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createProfile, getCurrentProfile } from '../../actions/profile';
 
+const initialState = {
+  company: '',
+  website: '',
+  location: '',
+  status: '',
+  skills: '',
+  githubusername: '',
+  bio: '',
+  twitter: '',
+  facebook: '',
+  linkedin: '',
+  youtube: '',
+  instagram: '',
+};
+
 const EditProfile = ({
   profile: { profile, loading },
   createProfile,
   getCurrentProfile,
   history,
 }) => {
-  const [formData, setFormData] = useState({
-    company: '',
-    website: '',
-    location: '',
-    status: '',
-    skills: '',
-    bio: '',
-    githubusername: '',
-    twitter: '',
-    facebook: '',
-    youtube: '',
-    linkedin: '',
-    instagram: '',
-  });
+  const [formData, setFormData] = useState(
+    initialState
+    // {
+    //   company: '',
+    //   website: '',
+    //   location: '',
+    //   status: '',
+    //   skills: '',
+    //   bio: '',
+    //   githubusername: '',
+    //   twitter: '',
+    //   facebook: '',
+    //   youtube: '',
+    //   linkedin: '',
+    //   instagram: '',
+    // }
+  );
 
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
 
   useEffect(() => {
-    getCurrentProfile();
+    if (!profile) getCurrentProfile();
+    if (!loading && profile) {
+      const profileData = { ...initialState };
+      for (const key in profile) {
+        if (key in profileData) profileData[key] = profile[key];
+      }
+      for (const key in profile.social) {
+        if (key in profileData) profileData[key] = profile.social[key];
+      }
+      if (Array.isArray(profileData.skills))
+        profileData.skills = profileData.skills.join(', ');
+      setFormData(profileData);
+    }
+  }, [loading, getCurrentProfile, profile]);
 
-    setFormData({
-      company: loading || !profile.company ? '' : profile.company,
-      website: loading || !profile.website ? '' : profile.website,
-      location: loading || !profile.location ? '' : profile.location,
-      status: loading || !profile.status ? '' : profile.status,
-      skills: loading || !profile.skills ? '' : profile.skills.join(','),
-      bio: loading || !profile.bio ? '' : profile.bio,
-      githubusername:
-        loading || !profile.social.githubusername
-          ? ''
-          : profile.social.githubusername,
-      twitter: loading || !profile.social.twitter ? '' : profile.social.twitter,
-      facebook:
-        loading || !profile.social.facebook ? '' : profile.social.facebook,
-      youtube: loading || !profile.social.youtube ? '' : profile.social.youtube,
-      linkedin:
-        loading || !profile.social.linkedin ? '' : profile.social.linkedin,
-      instagram:
-        loading || !profile.social.instagram ? '' : profile.social.instagram,
-    });
-  }, [loading, getCurrentProfile]);
+  // useEffect(() => {
+  //   getCurrentProfile();
+
+  //   setFormData({
+  //     company: loading || !profile.company ? '' : profile.company,
+  //     website: loading || !profile.website ? '' : profile.website,
+  //     location: loading || !profile.location ? '' : profile.location,
+  //     status: loading || !profile.status ? '' : profile.status,
+  //     skills: loading || !profile.skills ? '' : profile.skills.join(','),
+  //     bio: loading || !profile.bio ? '' : profile.bio,
+  //     githubusername:
+  //       loading || !profile.social.githubusername
+  //         ? ''
+  //         : profile.social.githubusername,
+  //     twitter: loading || !profile.social.twitter ? '' : profile.social.twitter,
+  //     facebook:
+  //       loading || !profile.social.facebook ? '' : profile.social.facebook,
+  //     youtube: loading || !profile.social.youtube ? '' : profile.social.youtube,
+  //     linkedin:
+  //       loading || !profile.social.linkedin ? '' : profile.social.linkedin,
+  //     instagram:
+  //       loading || !profile.social.instagram ? '' : profile.social.instagram,
+  //   });
+  // }, []);
 
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
